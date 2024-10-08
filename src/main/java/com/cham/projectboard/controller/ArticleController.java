f0344f8ad483111c4887c808d5c68de8b520a3c9
champark <97137143+champark@users.noreply.github.com>
2024-10-08 18:26:41 +0900

    #4 게시글 뷰 구현시작
    게시글 뷰 구현의 기본 골격 형성

--- src/main/java/com/cham/projectboard/controller/ArticleController.java
+++ src/main/java/com/cham/projectboard/controller/ArticleController.java
@@ -1,8 +1,12 @@
 package com.cham.projectboard.controller;
 
+import com.cham.projectboard.domain.Article;
+import com.cham.projectboard.domain.ArticleComment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -11,11 +15,25 @@ import java.util.List;
 @Controller
 public class ArticleController {
 
+/*    @Autowired
+    private Article article;
+    @Autowired
+    private ArticleComment articleComment;*/
+
     @GetMapping
     public String articles(ModelMap map) {
 
         map.addAttribute("articles", List.of());
 
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+
+        map.addAttribute("article", null);
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
     }
 }
