f7e17b21f1c400063b1dd988c77aeac5f8168c2e
champark <97137143+champark@users.noreply.github.com>
2024-10-13 00:39:29 +0900

    Delete src/main/java/com/cham/projectboard/repository/UserAccountRepository.java

--- src/main/java/com/cham/projectboard/repository/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cham.projectboard.repository;
-
-import com.cham.projectboard.domain.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-}
