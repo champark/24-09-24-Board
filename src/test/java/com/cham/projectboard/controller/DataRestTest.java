b2d8f2826675957ba27baca9bfdd25dcc7b974b1
champark <97137143+champark@users.noreply.github.com>
2024-10-13 22:43:31 +0900

    #7 계정에 도메인을 추가했다

--- src/test/java/com/cham/projectboard/controller/DataRestTest.java
+++ src/test/java/com/cham/projectboard/controller/DataRestTest.java
@@ -10,11 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Disabled("Spring Data REST 통합테스트 불필요하므로 제외시킨다")
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외시킴")
 @DisplayName("Data REST - API 테스트")
 @Transactional
 @AutoConfigureMockMvc
@@ -27,9 +27,10 @@ public class DataRestTest {
         this.mvc = mvc;
     }
 
+
     @DisplayName("[api] 게시글 리스트 조회")
     @Test
-    void givenNothing_whenRequestingArticles_thenResutrnsArticlesJsonResponse() throws Exception {
+    void givenNothing_whenRequestingArticles_thenReturnsArticlesJsonResponse() throws Exception {
         // Given
 
         // When & Then
@@ -40,7 +41,7 @@ public class DataRestTest {
 
     @DisplayName("[api] 게시글 단건 조회")
     @Test
-    void givenNothing_whenRequestingArticles_thenResutrnsArticleJsonResponse() throws Exception {
+    void givenNothing_whenRequestingArticle_thenReturnsArticleJsonResponse() throws Exception {
         // Given
 
         // When & Then
@@ -49,9 +50,9 @@ public class DataRestTest {
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
-    @DisplayName("[api] 게시글 댓글 리스트 조회")
+    @DisplayName("[api] 게시글 -> 댓글 리스트 조회")
     @Test
-    void givenNothing_whenRequestingArticles_thenResutrnsArticleCommentsJsonResponse() throws Exception {
+    void givenNothing_whenRequestingArticleCommentsFromArticle_thenReturnsArticleCommentsJsonResponse() throws Exception {
         // Given
 
         // When & Then
@@ -81,4 +82,19 @@ public class DataRestTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
+
+    @DisplayName("[api] 회원 관련 API 는 일체 제공하지 않는다.")
+    @Test
+    void givenNothing_whenRequestingUserAccounts_thenThrowsException() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(post("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(put("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(patch("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(delete("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(head("/api/userAccounts")).andExpect(status().isNotFound());
+    }
+
 }
